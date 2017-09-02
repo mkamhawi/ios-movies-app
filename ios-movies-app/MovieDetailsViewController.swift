3a432aef0dd1e1b2a4eefdd4469cfe736d8cb4ef
Mohamed Elkamhawi <mkamhawie@gmail.com>
2017-09-03 01:37:33 +0200

    Display the film's release date, trailers, and reviews in the MovieDetailsViewController.

--- ios-movies-app/MovieDetailsViewController.swift
+++ ios-movies-app/MovieDetailsViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 import CoreData
 import Kingfisher
 
-class MovieDetailsViewController: UIViewController {
+class MovieDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var movieId: Int64!
     var movieDetails: Movie!
+    var trailers: [Trailer]?
+    var reviews: [Review]?
     var networkOperations: NetworkOperations!
     @IBOutlet weak var movieTitle: UILabel!
     @IBOutlet weak var tagline: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
     @IBOutlet weak var overview: UILabel!
     @IBOutlet weak var moviePoster: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +44,8 @@ class MovieDetailsViewController: UIViewController {
             let matches = try AppDelegate.viewContext.fetch(request)
             assert(matches.count == 1, "MovieDetailsViewController-loadMovieDetails: database inconsistency")
             self.movieDetails = matches[0]
+            self.trailers = self.movieDetails.trailers?.allObjects as? [Trailer]
+            self.reviews = self.movieDetails.reviews?.allObjects as? [Review]
             displayMovieDetails()
         } catch {
             print("Error MovieDetailsViewController-loadMovieDetails: \(error)")
@@ -48,8 +54,11 @@ class MovieDetailsViewController: UIViewController {
     
     func displayMovieDetails() {
         if movieDetails != nil {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .medium
             movieTitle.text = movieDetails.title ?? ""
             tagline.text = movieDetails.tagline ?? ""
+            releaseDate.text = formatter.string(from: movieDetails.releaseDate! as Date) 
             overview.text = movieDetails.overview ?? ""
             if let posterPath = movieDetails.posterPath {
                 let posterUrl = URL(string: networkOperations.posterBaseUrl + posterPath)
@@ -58,6 +67,7 @@ class MovieDetailsViewController: UIViewController {
                 let posterUrl = URL(string: "http://via.placeholder.com/150x200")
                 moviePoster.kf.setImage(with: posterUrl)
             }
+            self.tableView.reloadData()
         }
     }
     
@@ -67,5 +77,53 @@ class MovieDetailsViewController: UIViewController {
                 self.loadMovieDetails()
             }
         }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        var sectionCount = 0
+        if let trailersCount = self.trailers?.count {
+            sectionCount += trailersCount > 0 ? 1 : 0
+        }
+        
+        if let reviewsCount = self.reviews?.count {
+            sectionCount += reviewsCount > 0 ? 1 : 0
+        }
+        return sectionCount
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            if let trailersCount = self.trailers?.count {
+                return trailersCount
+            }
+        }
+        
+        if let reviewsCount = self.reviews?.count {
+            return reviewsCount
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            if (self.trailers?.count) != nil {
+                return "Trailers"
+            }
+        }
+        return "Reviews"
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if indexPath.section == 0 {
+            if let trailersCount = self.trailers?.count, trailersCount > 0 {
+                cell?.textLabel?.text = self.trailers?[indexPath.row].name
+                return cell!
+            }
+        }
+        if let reviewsCount = self.reviews?.count, reviewsCount > 0 {
+            cell?.textLabel?.text = self.reviews?[indexPath.row].author
+        }
+        return cell!
     }
 }
